a955a0ace566408eed487d262a102e2d22d5c0bd
emerson-t <156133504+emerson-t@users.noreply.github.com>
2024-01-09 15:39:09 -0800

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+# Comment
+# This program will write Hello World!
+
+# This function tells the computer to print 'Hello World!'
+def PrintHelloWorld():
+	print("Hello World!")
+
+#This is the program that will be run by Pythn
+def main():
+	PrintHelloWorld()
+
+# Execute the main program when script is run
+if __name__=="__main__":
+    main()
