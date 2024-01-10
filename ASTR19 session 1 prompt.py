88d232662990eb284eb39d66192876273854b8b8
emerson-t <156133504+emerson-t@users.noreply.github.com>
2024-01-09 18:25:00 -0800

    Add files via upload
    Session 1 Prompt: Write a Python program to print out your full name, your preferred pronouns (optional), and two sentences about your favorite movie and your favorite food.

---
+++ ASTR19 session 1 prompt.py
@@ -0,0 +1,14 @@
+# ASTR19 session 1 prompt
+
+def PrintHello():
+	print("Hello! My name is Emerson Tao.")
+	print("My pronouns are he/him and they/them.")
+	print("My favorite movie is \'Interstellar\', since I love space. \nMy favorite food at the moment is mapo tofu.")
+
+# this tells our main() function
+def main():
+	PrintHello()
+
+# this prioritizes the exectution of main() when the script is run
+if __name__=="__main__":
+	main()
