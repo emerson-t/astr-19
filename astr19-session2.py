83ecaa617318f2019ea66cff737cac1ded13bc25
emerson-t <156133504+emerson-t@users.noreply.github.com>
2024-01-16 00:20:57 -0800

    Code Journal Session 2

---
+++ astr19-session2.py
@@ -0,0 +1,29 @@
+'''
+Write a Python program that prints the
+sum of two floating point numbers, the
+difference between two integers, and 
+the product of a floating point number 
+and an integer. In each case, have the
+program print out the data type of the
+resulting answer.
+'''
+
+#float point numbers
+
+x = 15.7    #define integer x equal to 15.7
+y = 8.3     #define integer y equal to 8.3
+
+#integers
+a = 13       #declare integer a equal to 4
+b = 7      #declare integer b equal to 10
+
+#arithmetic operators
+
+print("x+y = ",x+y)              #Addition
+print("type(x+y): ",type(x+y))   #print data type
+
+print("a-b = ",a-b)              #Subtraction
+print("type(a-b): ",type(a-b))   #print data type
+
+print("y*a = ",y*a)              #Multiplication
+print("type(y*a): ",type(y*a))   #print data type
