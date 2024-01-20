bc255500b80cec9282ef377ea392949165679a07
emerson-t <156133504+emerson-t@users.noreply.github.com>
2024-01-20 12:01:29 -0800

    Add files via upload

---
+++ astr19-session3.py
@@ -0,0 +1,22 @@
+import numpy as np       #import numpy with alias
+
+
+def f(x):                #define f(x)
+    return x**3 + 8      #returns the function
+
+
+def main():              #define main program
+    x = 9                #define integer x with the value 9
+    print("f(x) = x**3 + 8\n\nWhen x =",x,", f(x) =",f(x))
+#^^ printing the math done for f(x) to get the final result
+
+    if f(x) > 27:
+        print("\nYAY!")        #result if f(x) > 27
+
+    else:
+        print("\nTry again!")  #result if f(x) < 27
+
+
+#if the main() function exists, run it
+if __name__ == "__main__":
+    main()
